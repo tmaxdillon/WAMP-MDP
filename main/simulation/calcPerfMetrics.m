52617513fa6973307d725f00f89877275938f5c9
Trent Dillon <tmaxd@uw.edu>
2021-08-17 00:25:18 -0700

    added diagnostics to calcPerfMetrics

--- main/simulation/calcPerfMetrics.m
+++ main/simulation/calcPerfMetrics.m
@@ -22,13 +22,14 @@ for i = 1:length(output.E_sim)
     [~,E_sim_ind(i)] =  min(abs(amp.E - output.E_sim(i)));
 end
 %find f extent based on action timeseries
-f_ext = find(output.a_sim > 0,1,'last');
+f_ext = find(output.a_act_sim > 0,1,'last');
 %reconstructed E timeseries and J timesries
 E_recon = zeros(length(output.E_sim),1);
 J_recon = zeros(length(output.E_sim),1);
 E_recon(1) = amp.E_start;
-% disp(['length f_ext = ' num2str(f_ext)])
-% disp(['WEC B = ' num2str(wec.B) ' and E_max = ' num2str(amp.E_max)])
+disp(['length f_ext = ' num2str(f_ext)])
+disp(['power averages not equal. parameter = ' ...
+        sim.tp{ceil(i/sim.n)} ' and value = ' num2str(sim.S1(i))])
 for f = 1:f_ext
 %     [~,E_recon(f+1)] = powerToBattery(output.Pw_sim(f), ...
 %         E_recon(f),amp.Ps(output.a_sim(f)), ...
