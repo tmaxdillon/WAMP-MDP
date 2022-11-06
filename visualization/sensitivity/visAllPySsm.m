7a7ad2fd7d7041414bea12063c47cc729ad5d66e
Trent Dillon <tmaxd@uw.edu>
2022-11-05 21:56:44 -0700

    new run of tpe tam and dfr sens

---
+++ visualization/sensitivity/visAllPySsm.m
@@ -0,0 +1,15 @@
+function [] = visAllPySsm(var)
+
+visPySsm(var,'com',1,2)
+visPySsm(var,'com',1,1)
+visPySsm(var,'com',0,2)
+visPySsm(var,'com',0,1)
+visPySsm(var,'int',1,2)
+visPySsm(var,'int',1,1)
+visPySsm(var,'int',0,2)
+visPySsm(var,'int',0,1)
+visPySsm(var,'pow',1,[])
+visPySsm(var,'pow',0,[])
+
+end
+
